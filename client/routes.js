f30c33e3ac0ab1245d8bb93af70acf20f28b7781
tmaioli <tmaioli@deloitte.it>
2024-03-07 14:52:37 +0100

    first commit

---
+++ client/routes.js
@@ -0,0 +1,13 @@
+import Index from "./pages/index.jsx";
+import Other from "./pages/other.jsx";
+
+export default [
+  {
+    path: "/",
+    component: Index,
+  },
+  {
+    path: "/other",
+    component: Other,
+  },
+];
